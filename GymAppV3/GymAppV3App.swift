14a3b1a818912edc2408ead644bb3b26ecca225c
theofarris <theoisawesome1@gmail.com>
2024-04-18 13:13:40 -0700

    Initial Commit

---
+++ GymAppV3/GymAppV3App.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppV3App.swift
+//  GymAppV3
+//
+//  Created by Theo Farris on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GymAppV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymAppV3UITests/GymAppV3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GymAppV3UITestsLaunchTests.swift
+//  GymAppV3UITests
+//
+//  Created by Theo Farris on 4/18/24.
+//
+
+import XCTest
+
+final class GymAppV3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
